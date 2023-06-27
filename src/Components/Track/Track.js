83df035f630dbc1e653984c1ff4b0e84a9076d8e
Chris Mastris <cmastris@hotmail.com>
2023-06-27 22:38:16 +0100

    Dynamically render track action button content

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,13 +1,18 @@
 import './Track.css';
 
-function Track() {
+function Track({ isRemoval }) {
+
+  function renderAction() {
+    return isRemoval ? "-" : "+";
+  }
+
   return (
     <div className="Track">
       <div className="Track-information">
         <h3>Track Name</h3>
         <p>Track Artist | Track Album</p>
       </div>
-      <button className="Track-action">+ or -</button>
+      <button className="Track-action">{renderAction()}</button>
     </div>
   );
 }
