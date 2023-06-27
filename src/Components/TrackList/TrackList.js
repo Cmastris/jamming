c0420a9c5a743bbc8b842f7c9bae4f53c7c41362
Chris Mastris <cmastris@hotmail.com>
2023-06-27 22:07:48 +0100

    Add TrackList component

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,9 @@
+function TrackList() {
+  return (
+    <div className="TrackList">
+      {/* Add a map method that renders a set of Track components */}
+    </div>
+  );
+}
+  
+export default TrackList;
