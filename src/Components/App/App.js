f2c372704c390d234fc086fa54f53a00a638f75c
Chris Mastris <cmastris@hotmail.com>
2023-06-27 23:18:37 +0100

    Include SearchBar within App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
@@ -7,7 +8,7 @@ function App() {
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        {/* Add a SearchBar component */}
+        <SearchBar />
         <div className="App-playlist">
           <SearchResults />
           <Playlist />
