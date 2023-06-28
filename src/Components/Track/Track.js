36e1825b5002eb59f978b0ffbedce002c4137cf0
Chris Mastris <cmastris@hotmail.com>
2023-06-28 22:54:24 +0100

    Refactor Track action button rendering

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,10 +2,6 @@ import './Track.css';
 
 function Track({ track, name, artist, album, isRemoval, onAdd }) {
 
-  function renderAction() {
-    return isRemoval ? "-" : "+";
-  }
-
   function addTrack() {
     onAdd(track);
   }
@@ -16,7 +12,9 @@ function Track({ track, name, artist, album, isRemoval, onAdd }) {
         <h3>{name}</h3>
         <p>{artist} | {album}</p>
       </div>
-      <button className="Track-action" onClick={addTrack}>{renderAction()}</button>
+      <button className="Track-action" onClick={isRemoval ? null : addTrack}>
+        {isRemoval ? "-" : "+"}
+      </button>
     </div>
   );
 }
